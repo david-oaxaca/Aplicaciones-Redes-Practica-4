7212cfef45cf14476d1f177a8d3990a8299d7405
tdwda <tdwda@LAPTOP-CLED55LT>
2021-05-16 10:24:25 -0500

    v 1.5 Cambio de bufferedreader

--- src/ServidorWeb.java
+++ src/ServidorWeb.java
@@ -9,9 +9,10 @@ public class ServidorWeb {
 
     public static final int PUERTO = 8000;
     ServerSocket ss;
-    protected boolean detenido = false;
-    protected Thread runningThread = null;
     protected ExecutorService pool = Executors.newFixedThreadPool(5);
+    
+        
+        
 
     class Manejador extends Thread {
 
@@ -28,135 +29,171 @@ public class ServidorWeb {
         public void run() {
             try {
                 //DataInputStream dis= new DataInputStream(socket.getInputStream());
-                br = new BufferedReader(new InputStreamReader(socket.getInputStream()/*, "windows-1252"*/));
+                //br = new BufferedReader(new InputStreamReader(socket.getInputStream(), "ISO-8859-1"/*, "windows-1252"*/));
+                
+                DataInputStream dis = new DataInputStream(socket.getInputStream());
+                
                 bos = new BufferedOutputStream(socket.getOutputStream());
                 pw = new PrintWriter(new OutputStreamWriter(bos));
-                String line = br.readLine();
-                //System.out.println(line);
-                if (line == null) {
-                    pw.print("<html><head><title>Servidor WEB");
-                    pw.print("</title><body bgcolor=\"#AACCFF\"<br>Linea Vacia</br>");
-                    pw.print("</body></html>");
-                    socket.close();
-                    return;
-                }
-                System.out.println("\nCliente Conectado desde: " + socket.getInetAddress());
-                System.out.println("Por el puerto: " + socket.getPort());
-                System.out.println("Datos: " + line + "\r\n\r\n");
+                
+                
+                byte[] byteData = new byte[65535];
+                int n = dis.read(byteData);
+                System.out.println("Tamaño de lo leido: " + n);
+                String datos = "";
+                if(n != -1){
+                    datos = new String(byteData, 0, n, "ISO-8859-1");
+                    //String firstLine = datos.split("\n")[0];
+                    String line = datos.split("\n")[0];
+                    //System.out.println(line);
+                    if (line == null) {
+                        pw.print("<html><head><title>Servidor WEB");
+                        pw.print("</title><body bgcolor=\"#AACCFF\"<br>Linea Vacia</br>");
+                        pw.print("</body></html>");
+                        socket.close();
+                        return;
+                    }
+                    System.out.println("\nCliente Conectado desde: " + socket.getInetAddress());
+                    System.out.println("Por el puerto: " + socket.getPort());
+                    System.out.println("Datos: " + line + "\r\n\r\n");
 
-                if (!line.contains("?")) {
-                    if (line.toUpperCase().startsWith("POST")) {
-                        StringBuilder constructor = new StringBuilder();
-                        while (br.ready()) {
-                            constructor.append((char) br.read());
+                    if (!line.contains("?")) {
+                        if (line.toUpperCase().startsWith("POST")) {
+
+                            //System.out.println("POST content: " + constructor);
+                            String[] tokens = datos.split("\n\r");
+                            int len = tokens.length;
+                            System.out.println("Token:");
+                            String req = tokens[len - 1];
+                            System.out.println(req);
+
+                            /*
+                                StringBuilder constructor = new StringBuilder();
+                                while (br.ready()) {
+                                    constructor.append((char) br.read());
+                                }
+                            */                        
+                            pw.println("HTTP/1.0 200 Okay");
+                            pw.flush();
+                            pw.println();
+                            //pw.println();
+                            pw.flush();
+                            pw.print("<html><head><title>SERVIDOR WEB");
+                            pw.flush();
+                            pw.print("</title></head><body bgcolor=\"#FFCDD2\"><center><h1><br>Parametros Obtenidos..</br></h1>");
+                            pw.flush();
+                            pw.print("<h3><b>" + req + "</b></h3>");
+                            pw.flush();
+                            pw.print("</center></body></html>");
+                            pw.flush();
+                        } else if (line.toUpperCase().startsWith("PUT")) {
+                            getArch(line);
+                            StringBuilder constructor = new StringBuilder();
+
+                            /*
+                                n = tamaño de datos en bits
+
+                            */
+
+                            String [] parsingHeader = datos.split("\r\n\r\n");
+                            
+                            int tamHeader = (parsingHeader[0] + "\r\n\r\n").getBytes("ISO-8859-1").length;
+                            int filePart = parsingHeader[parsingHeader.length - 1].getBytes().length;
+
+                            byte[] contenido = parsingHeader[parsingHeader.length - 1].getBytes("ISO-8859-1");
+
+                            /*for (int i = tamHeader; i < n; i++) {
+                                contenido[i - tamHeader] = byteData[i];
+                            }*/
+                            //System.arraycopy(byteData, tamHeader, contenido, 0, n - tamHeader);
+
+                            System.out.println("Tamaño del header: " + tamHeader);
+                            
+                            System.out.println("Tamaño de la parte del archivo: " + filePart);
+                            
+                            System.out.println("Tamaño del arreglo de datos: " + contenido.length);
+                            
+                            System.out.println("Tamaño de datos recibidos: " +  n);
+                            
+                            //String contenido_archivo = "";
+                            /*while (br.ready()) {
+                                //n= dis.read(tmp);
+                                constructor.append((char)br.read());
+                                //plstr+= new String(tmp,0,n);
+                            }*/
+                            //System.out.println(constructor);
+                           /* String plstr = constructor.toString();
+                            String[] tokens = plstr.split("\r\n\r\n");
+                            imprimirTokens(tokens);
+
+                            int len = tokens.length;
+                            String contenido_archivo = tokens[len - 1].split("\n--------")[0];
+                            System.out.println("Nombre archivo:" + FileName + ", Contenido: \n" + contenido_archivo);
+                            */
+                            File archivo = crearArchivo(FileName);
+                            archivo.createNewFile();
+
+
+
+                            try (FileOutputStream fos = new FileOutputStream(archivo)) {
+
+                                fos.write(contenido);
+                                System.out.println("Archivo transferido....");
+                            }catch (IOException e) {
+                                e.printStackTrace();
+                            }
+
+                            pw.println("HTTP/1.0 201 Created");
+                            pw.flush();
+                            pw.println();
+                            pw.flush();
+                        } else {
+                            getArch(line);
+                            if (FileName.compareTo("") == 0) {
+                                SendA("index.htm");
+                            } else {
+                                SendA(FileName);
+                            }
+                            System.out.println(FileName);
                         }
-                        //System.out.println("POST content: " + constructor);
-                        String[] tokens = constructor.toString().split("\n\r");
-                        System.out.println("Tokens:");
-                        //StringTokenizer tokens=new StringTokenizer(constructor.toString(),"\n");
-                        //String req_a = line;
-                        String req = "";
-                        //System.out.println("AAAAAAAAAAA");
-                        imprimirTokens(tokens);
-                        int i;
-                        for(i= 1; i < tokens.length; i++) {
-                            //if(i%2 == 0) {
-                                req+= tokens[i];
-                                System.out.println(tokens[i]);
-                            //}
-                        }
+                    } else if (line.toUpperCase().startsWith("GET")) {
+                        StringTokenizer tokens = new StringTokenizer(line, "?");
+                        String req_a = tokens.nextToken();
+                        String req = tokens.nextToken();
+                        System.out.println("Token1: " + req_a + "\r\n\r\n");
+                        System.out.println("Token2: " + req + "\r\n\r\n");
                         pw.println("HTTP/1.0 200 Okay");
                         pw.flush();
                         pw.println();
-                        //pw.println();
                         pw.flush();
                         pw.print("<html><head><title>SERVIDOR WEB");
                         pw.flush();
-                        pw.print("</title></head><body bgcolor=\"#FFCDD2\"><center><h1><br>Parametros Obtenidos..</br></h1>");
+                        pw.print("</title></head><body bgcolor=\"#AACCFF\"><center><h1><br>Parametros Obtenidos..</br></h1>");
                         pw.flush();
                         pw.print("<h3><b>" + req + "</b></h3>");
                         pw.flush();
                         pw.print("</center></body></html>");
                         pw.flush();
-                    } else if (line.toUpperCase().startsWith("PUT")) {
-                        getArch(line);
-                        StringBuilder constructor = new StringBuilder();
-                        
-                        //int n= 65535;
-                        //byte[] tmp= new byte[749721];
-                        //String plstr= "";
-                        while (br.ready()) {
-                            //n= dis.read(tmp);
-                            constructor.append((char)br.read());
-                            //plstr+= new String(tmp,0,n);
-                        }
-                        //System.out.println(constructor);
-                        String plstr = constructor.toString();
-                        String[] tokens = plstr.split("\r\n\r\n");
-                        imprimirTokens(tokens);
-                        
-                        int len = tokens.length;
-                        String contenido_archivo = tokens[len - 1].split("\n--------")[0];
-                        System.out.println("Nombre archivo:" + FileName + ", Contenido: \n" + contenido_archivo);
-                        File archivo = new File(FileName);
-                        archivo.createNewFile();
-                        
-                        FileWriter escritor = new FileWriter(archivo);
-                        int i;
-                        for(i= 2; i < tokens.length-1; i++) {
-                            escritor.write(tokens[i] + "\r\n\r\n");
-                        }
-                        escritor.write(contenido_archivo);
-                        escritor.close();
-
-                        pw.println("HTTP/1.0 201 Created");
+                    } else if (line.toUpperCase().startsWith("HEAD")) {
+                        StringTokenizer tokens = new StringTokenizer(line, "?");
+                        String req_a = tokens.nextToken();
+                        String req = tokens.nextToken();
+                        System.out.println("Token1: " + req_a + "\r\n\r\n");
+                        System.out.println("Token2: " + req + "\r\n\r\n");
+                        pw.println("HTTP/1.0 200 Okay");
                         pw.flush();
                         pw.println();
                         pw.flush();
+                        // No mandas el archivo, solo el encabezado
                     } else {
-                        getArch(line);
-                        if (FileName.compareTo("") == 0) {
-                            SendA("index.htm");
-                        } else {
-                            SendA(FileName);
-                        }
-                        System.out.println(FileName);
+                        pw.println("HTTP/1.0 501 Not Implemented");
+                        pw.println();
                     }
-                } else if (line.toUpperCase().startsWith("GET")) {
-                    StringTokenizer tokens = new StringTokenizer(line, "?");
-                    String req_a = tokens.nextToken();
-                    String req = tokens.nextToken();
-                    System.out.println("Token1: " + req_a + "\r\n\r\n");
-                    System.out.println("Token2: " + req + "\r\n\r\n");
-                    pw.println("HTTP/1.0 200 Okay");
                     pw.flush();
-                    pw.println();
-                    pw.flush();
-                    pw.print("<html><head><title>SERVIDOR WEB");
-                    pw.flush();
-                    pw.print("</title></head><body bgcolor=\"#AACCFF\"><center><h1><br>Parametros Obtenidos..</br></h1>");
-                    pw.flush();
-                    pw.print("<h3><b>" + req + "</b></h3>");
-                    pw.flush();
-                    pw.print("</center></body></html>");
-                    pw.flush();
-                } else if (line.toUpperCase().startsWith("HEAD")) {
-                    StringTokenizer tokens = new StringTokenizer(line, "?");
-                    String req_a = tokens.nextToken();
-                    String req = tokens.nextToken();
-                    System.out.println("Token1: " + req_a + "\r\n\r\n");
-                    System.out.println("Token2: " + req + "\r\n\r\n");
-                    pw.println("HTTP/1.0 200 Okay");
-                    pw.flush();
-                    pw.println();
-                    pw.flush();
-                    // No mandas el archivo, solo el encabezado
-                } else {
-                    pw.println("HTTP/1.0 501 Not Implemented");
-                    pw.println();
+                    bos.flush();
                 }
-                pw.flush();
-                bos.flush();
+                
+                
             } catch (Exception e) {
                 e.printStackTrace();
             }
@@ -297,37 +334,41 @@ public class ServidorWeb {
                 System.out.println(e.getMessage());
             }
         }
+        
+            private void verificarCarpeta(File carpeta) {
+                if(!carpeta.getParentFile().exists()){
+                    verificarCarpeta(carpeta.getParentFile());
+                }
+
+                if(!carpeta.exists()) {
+                    carpeta.mkdir();
+                }
+            }
+        
+        public File crearArchivo(String nombre) {
+            String ruta_madre= new File("").getAbsolutePath();
+            String ruta_servidor= ruta_madre + "/Uploads";
+            File carpeta = new File(ruta_servidor);
+            //verificarCarpeta(new File(ruta_servidor)); //Verificamos que previamente exista la ruta del servidor
+            verificarCarpeta(carpeta);
+            return new File(ruta_servidor + "\\" + nombre);
+        }
     }//Fin de Manejador
 
     public ServidorWeb() throws Exception {
-        synchronized (this) {
-            this.runningThread = Thread.currentThread();
-        }
+        
         System.out.println("Iniciando Servidor.......");
         this.ss = new ServerSocket(PUERTO);
         System.out.println("Servidor iniciado:---OK");
         System.out.println("Esperando por Cliente....");
 
-        while (!detenido()) {
+        for(;;) {
             Socket accept = ss.accept();
             this.pool.execute(new Manejador(accept));
         }
-        this.pool.shutdown();
-        System.out.println("Servidor detenido.");
     }
 
-    private synchronized boolean detenido() {
-        return this.detenido;
-    }
-
-    public synchronized void stop() {
-        this.detenido = true;
-        try {
-            this.ss.close();
-        } catch (IOException e) {
-            throw new RuntimeException("Error al cerrar el socket del servidor", e);
-        }
-    }
+    
 
     public static void main(String[] args) throws Exception {
         ServidorWeb sWEB = new ServidorWeb();
